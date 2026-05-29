f77d1b50d49dbb133d2423734dc4d981edd091b6
mixteko <mixteko@gmail.com>
2026-05-29 17:12:14 -0600

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,84 @@
+const CACHE_NAME = "pedicalc-v1.73";
+
+const urlsToCache = [
+  "./",
+  "./index.html",
+  "./style.css",
+  "./app.js",
+  "./manifest.json"
+];
+
+self.addEventListener(
+  "install",
+  event => {
+
+    event.waitUntil(
+
+      caches.open(CACHE_NAME)
+      .then(cache => {
+
+        return cache.addAll(
+          urlsToCache
+        );
+
+      })
+
+    );
+
+  }
+);
+
+self.addEventListener(
+  "fetch",
+  event => {
+
+    event.respondWith(
+
+      caches.match(
+        event.request
+      )
+      .then(response => {
+
+        return response ||
+        fetch(event.request);
+
+      })
+
+    );
+
+  }
+);
+
+self.addEventListener(
+  "activate",
+  event => {
+
+    event.waitUntil(
+
+      caches.keys()
+      .then(keys => {
+
+        return Promise.all(
+
+          keys.map(key => {
+
+            if(
+              key !== CACHE_NAME
+            ){
+
+              return caches.delete(
+                key
+              );
+
+            }
+
+          })
+
+        );
+
+      })
+
+    );
+
+  }
+);
