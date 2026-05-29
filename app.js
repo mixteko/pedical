cdf1a49000114a70577ffbe2b6fd0d6fa3ab434c
mixteko <mixteko@gmail.com>
2026-05-29 13:51:36 -0600

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+alert("APP CARGADA");
 const SHEET_URL =
 "https://docs.google.com/spreadsheets/d/e/2PACX-1vRDl2i7g0UIQW5Jw6dKXmStMBcrYrcsJ2Kxd0dinPfV75Wjoe0z-fxFBQLtxMsD3A8Xby3DpbKmPXMU/pub?gid=0&single=true&output=csv";
 
